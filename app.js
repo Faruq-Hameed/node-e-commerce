bbb62574f227745c1314a51e314f3b6b10b58b97
Faruq-Hameed <faruqhameed1@gmail.com>
2023-04-28 10:11:08 +0100

    allowed CORS testing

--- app.js
+++ app.js
@@ -1,50 +1,61 @@
-const express = require('express')
-const morgan = require('morgan');
-const helmet = require('helmet');
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
 
-const {db_connection,URI} = require('./database')
-const {users, items, cart, payment, wallet} = require('./routes')
+const { db_connection, URI } = require("./database");
+const { users, items, cart, payment, wallet } = require("./routes");
 
+require("dotenv").config({ path: "./.env" });
 
-require('dotenv').config({path: './.env'})
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 const app = express();
 
 db_connection()
-    .then(result => {
-        console.log('connected to database successfully')
-    }).catch(err => {
-        console.log('error connecting to database:', err.message)
-        process.exit(1)
-    })
+  .then((result) => {
+    console.log("connected to database successfully");
+  })
+  .catch((err) => {
+    console.log("error connecting to database:", err.message);
+    process.exit(1);
+  });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms - :remote-user :date"
+  )
+);
+app.use(helmet());
+// app.use(cors({
+//     origin: 'https://www.section.io'
+// }));
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms - :remote-user :date"))
-app.use(helmet())
+/**allow more than one cross-origin-request-sharing(CORS) */
+app.use(
+  cors({
+    origin: ["https://www.section.io", "https://www.google.com/"],
+  })
+);
+app.use("/api/users", users);
+app.use("/api/items", items);
+app.use("/api/cart", cart);
+app.use("/api/payments", payment);
+app.use("/api/wallet", wallet);
 
+app.use("/static", express.static("./public")); //added virtual prefix('/static'. It is needed to get public files directly)
 
-app.use('/api/users', users)
-app.use('/api/items', items)
-app.use('/api/cart', cart)
-app.use('/api/payments', payment)
-app.use('/api/wallet', wallet)
-
-app.use('/static',express.static('./public')) //added virtual prefix('/static'. It is needed to get public files directly)
-
-app.get('/', (req, res)=>{
-
-    // res.render('index')
-    res.status(200).send("<h1>Hello world</h1>")
-})
+app.get("/", (req, res) => {
+  // res.render('index')
+  res.status(200).send("<h1>Hello world</h1>");
+});
 
 //handling all unknown url requests.
 app.use(function (req, res, next) {
-    res.status(404).send("Sorry can't find that! Request failed")
-  });
+  res.status(404).send("Sorry can't find that! Request failed");
+});
 
-app.listen(port, ()=>{
-    console.info('listening on port ', port)
-})
+app.listen(port, () => {
+  console.info("listening on port ", port);
+});
